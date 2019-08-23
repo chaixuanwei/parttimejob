375839a3d1dcaee0d115167ebd76fbd490080574
chaixuanwei <1247389232@qq.com>
2019-08-23 11:57:15 +0800

    添加效果图

--- app/src/main/java/com/example/myapplication/me/activity/AmendActivity.java
+++ app/src/main/java/com/example/myapplication/me/activity/AmendActivity.java
@@ -1,23 +1,17 @@
 package com.example.myapplication.me.activity;
 
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
-import android.util.DisplayMetrics;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.config.Config;
@@ -25,28 +19,42 @@ import com.example.myapplication.frame.BaseMvpActivity;
 import com.example.myapplication.frame.CommonPresenter;
 import com.example.myapplication.local_utils.PhotoUtils;
 import com.example.myapplication.local_utils.SharedPrefrenceUtils;
+import com.example.myapplication.me.adapter.PhotoAdapter;
 import com.example.myapplication.model.MeModel;
 import com.example.myapplication.view.RoundImage;
+import com.jph.takephoto.app.TakePhoto;
+import com.jph.takephoto.app.TakePhotoImpl;
+import com.jph.takephoto.compress.CompressConfig;
+import com.jph.takephoto.model.CropOptions;
+import com.jph.takephoto.model.InvokeParam;
+import com.jph.takephoto.model.TContextWrap;
+import com.jph.takephoto.model.TImage;
+import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.permission.InvokeListener;
+import com.jph.takephoto.permission.PermissionManager;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-import me.nereo.multi_image_selector.MultiImageSelector;
-import me.nereo.multi_image_selector.MultiImageSelectorActivity;
 
-public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> {
+public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> implements InvokeListener, TakePhoto.TakeResultListener, PhotoAdapter.OnPhotoClick {
 
     protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101;
     @BindView(R.id.back)
     ImageView back;
     @BindView(R.id.headportrait)
     RoundImage headportrait;
-    @BindView(R.id.project_photo)
-    TextView projectPhoto;
+    @BindView(R.id.photo_rv)
+    RecyclerView photoRv;
+    private ArrayList<String> mList = new ArrayList<>();
+    private ArrayList<File> mFiles = new ArrayList<>();
+    private PhotoAdapter mAdapter;
+    private InvokeParam invokeParam;
+    private TakePhotoImpl mTakePhoto;
+    private int num = 0;
 
     @Override
     public int getLayoutId() {
@@ -55,9 +63,26 @@ public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> {
 
     @Override
     public void initView() {
+        initPermmition();
         Bitmap mBitmap = SharedPrefrenceUtils.getBitmap(this, Config.BITMAP, null);
         if (null != mBitmap) {
             headportrait.setImageBitmap(mBitmap);
+        }
+        LinearLayoutManager mManager = new LinearLayoutManager(this);
+        mManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mAdapter = new PhotoAdapter(this, mList);
+        photoRv.setLayoutManager(mManager);
+        photoRv.setAdapter(mAdapter);
+        mAdapter.setListener(this);
+    }
+
+    private void initPermmition() {
+        int permission = ActivityCompat.checkSelfPermission(AmendActivity.this,
+                Manifest.permission.CAMERA);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AmendActivity.this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
         }
     }
 
@@ -86,7 +111,7 @@ public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> {
 
     }
 
-    @OnClick({R.id.back, R.id.headportrait, R.id.project_photo})
+    @OnClick({R.id.back, R.id.headportrait})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.back:
@@ -97,54 +122,6 @@ public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> {
                 oneintent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
                 startActivityForResult(oneintent, Config.PHOTO);
                 break;
-            case R.id.project_photo:
-                pickImage();
-                break;
-        }
-    }
-
-    private void pickImage() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN // Permission was added in API Level 16
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
-                    getString(R.string.mis_permission_rationale),
-                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
-        } else {
-            int maxNum = 9;
-            MultiImageSelector selector = MultiImageSelector.create(AmendActivity.this);
-            selector.count(maxNum);
-            selector.multi();
-            selector.start(AmendActivity.this, Config.MOREPHOTO);
-        }
-    }
-
-    private void requestPermission(final String permission, String rationale, final int requestCode) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-            new AlertDialog.Builder(this)
-                    .setTitle(R.string.mis_permission_dialog_title)
-                    .setMessage(rationale)
-                    .setPositiveButton(R.string.mis_permission_dialog_ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(AmendActivity.this, new String[]{permission}, requestCode);
-                        }
-                    })
-                    .setNegativeButton(R.string.mis_permission_dialog_cancel, null)
-                    .create().show();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_STORAGE_READ_ACCESS_PERMISSION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                pickImage();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -166,29 +143,58 @@ public class AmendActivity extends BaseMvpActivity<CommonPresenter, MeModel> {
                     headportrait.setImageBitmap(mBitmap);
                 }
             }
-        } else if (requestCode == Config.MOREPHOTO) {
-//            if (data != null) {
-//                ArrayList<String> mSelectPath = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
-//                projectPhoto.setVisibility(View.GONE);
-//                for (int i = 0; i < mSelectPath.size(); i++) {
-//                    if (i == 0) {
-//                        Uri mUri = Uri.parse(mSelectPath.get(0));
-//                        imageOne.setImageURI(mUri);
-//                    } else if (i == 1) {
-//                        Uri mUri = Uri.parse(mSelectPath.get(1));
-//                        imageTwo.setImageURI(mUri);
-//                    } else if (i == 2) {
-//                        Uri mUri = Uri.parse(mSelectPath.get(2));
-//                        imageThree.setImageURI(mUri);
-//                    } else if (i == 3) {
-//                        Uri mUri = Uri.parse(mSelectPath.get(3));
-//                        imageFour.setImageURI(mUri);
-//                    } else if (i == 4) {
-//                        Uri mUri = Uri.parse(mSelectPath.get(4));
-//                        imageFive.setImageURI(mUri);
-//                    }
-//                }
-//            }
+        } else {
+            mTakePhoto.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //以下代码为处理Android6.0、7.0动态权限所需
+        PermissionManager.TPermissionType type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionManager.handlePermissionsResult(this, type, invokeParam, this);
+    }
+
+    @Override
+    public void takeSuccess(TResult result) {
+        ArrayList<TImage> mImages = result.getImages();
+        for (int i = 0; i < mImages.size(); i++) {
+            String mOriginalPath = mImages.get(i).getOriginalPath();
+            mList.add(mOriginalPath);
+            mFiles.add(new File(mOriginalPath));
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+
+    }
+
+    @Override
+    public void takeCancel() {
+
+    }
+
+    @Override
+    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
+        PermissionManager.TPermissionType type = PermissionManager.checkPermission(TContextWrap.of(this), invokeParam.getMethod());
+        if (PermissionManager.TPermissionType.WAIT.equals(type)) {
+            this.invokeParam = invokeParam;
+        }
+        return type;
+    }
+
+    @Override
+    public void onPhoto(int pI) {
+        num = pI;
+//        if (pI == 0) {
+        mList.clear();
+        mFiles.clear();
+        mTakePhoto = new TakePhotoImpl(this, this);
+        mTakePhoto.onEnableCompress(new CompressConfig.Builder().setMaxSize(50 * 1024).setMaxPixel(1080).create(), true);
+        mTakePhoto.onPickMultiple(5);
+//        }
     }
 }
