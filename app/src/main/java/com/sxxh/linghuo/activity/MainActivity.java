e2eb8a8f4cda9a045ef65cd60b646bb9f26efcc3
cheng <1977128452@qq.com>
2019-09-20 11:51:19 +0800

    添加退出提醒 修改首页搜索栏背景色

--- app/src/main/java/com/sxxh/linghuo/activity/MainActivity.java
+++ app/src/main/java/com/sxxh/linghuo/activity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.sxxh.linghuo.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
+import android.widget.FrameLayout;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.sxxh.linghuo.R;
 import com.sxxh.linghuo.config.Config;
 import com.sxxh.linghuo.customs.MyBottomView;
@@ -21,20 +25,37 @@ import com.sxxh.linghuo.model.HomeModel;
 import com.sxxh.linghuo.issus.fragment.IssusFragment;
 import com.sxxh.linghuo.me.fragment.MeFragment;
 import com.sxxh.linghuo.message.fragment.MessageFragment;
-import com.yanzhenjie.alertdialog.AlertDialog;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.PermissionListener;
 
 import java.util.List;
 
+import butterknife.BindView;
+
 public class MainActivity extends BaseMvpActivity<CommonPresenter, HomeModel> implements MyBottomView.OnBottomClick {
 
+    @BindView(R.id.frame_layout)
+    FrameLayout frameLayout;
     private MyBottomView mBottomView;
     private final int FRISTPAGE = 1;
     private final int ISSUS = 2;
     private final int MESSAGE = 3;
     private final int ME = 4;
     private FragmentManager mManager;
+    private long touchTime = 0;
+
+    //应用退出提醒
+    @Override
+    public void onBackPressed() {
+        // 模板自动生成的，大概是说如果左侧抽屉栏被打开，按返回键的时候关闭抽屉栏而不是退出程序
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - touchTime > 2000) {
+            Snackbar.make(frameLayout, "再按一次退出程序", Snackbar.LENGTH_SHORT).show();
+            touchTime = currentTime;
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     public int getLayoutId() {
@@ -53,7 +74,7 @@ public class MainActivity extends BaseMvpActivity<CommonPresenter, HomeModel> im
 
                         // 这里的requestCode就是申请时设置的requestCode。
                         // 和onActivityResult()的requestCode一样，用来区分多个不同的请求。
-                        if(requestCode == 200) {
+                        if (requestCode == 200) {
                             // TODO ...
                         }
                     }
@@ -61,7 +82,7 @@ public class MainActivity extends BaseMvpActivity<CommonPresenter, HomeModel> im
                     @Override
                     public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
                         // 权限申请失败回调
-                        if(requestCode == 200) {
+                        if (requestCode == 200) {
                         }
                     }
                 }).start();
@@ -93,7 +114,7 @@ public class MainActivity extends BaseMvpActivity<CommonPresenter, HomeModel> im
     }
 
     @Override
-    public void onError(int whichApi,Throwable e) {
+    public void onError(int whichApi, Throwable e) {
         netErrorToast(e);
     }
 
@@ -145,4 +166,36 @@ public class MainActivity extends BaseMvpActivity<CommonPresenter, HomeModel> im
         }
         fragmentTransaction.commit();
     }
+
+
+    /*//应用退出提醒
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            dialog();
+        }
+        return false;
+    }
+
+    private void dialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("提示");
+        dialog.setMessage("是否退出当前程序？");
+        dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                System.exit(0);
+            }
+        });
+        dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ToastUtils.showShort("取消");
+            }
+        });
+        if (dialog != null) {
+            dialog.show();
+        }
+    }*/
+
 }
